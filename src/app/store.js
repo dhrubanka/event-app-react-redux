5084786e24ee526b8213e20ccd5fb9ab7c0dfe0f
Dhrubanka Chutia <dhrubankachutia@gmail.com>
2022-12-09 20:15:23 +0530

    readme

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import profileReducer from '../features/profile/profileSlice';
 import eventReducer from '../features/event/eventSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     profile: profileReducer,
     event: eventReducer
   },
